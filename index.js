18f8d933e9730d09ed37e135d1ba39fcce9e0ff0
komissarovev <john_d@mail.ru>
2022-06-14 10:36:45 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+// const port = process.env.port ?? 3000
+const port = 80
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send('<h1>hello exp</h1>')
+})
+
+app.listen(port, ()=>{
+    console.log(`Server has been started on port ${port}...`)
+})
